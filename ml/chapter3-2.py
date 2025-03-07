5ae53bd4b34cfd9ae5b00c0bc7a78c96193e418c
kth232 <kth7537@naver.com>
2025-03-08 02:25:19 +0900

    선형 회귀귀

--- ml/chapter3-2.py
+++ ml/chapter3-2.py
@@ -20,10 +20,10 @@ perch_weight = np.array(
      1000.0, 1000.0]
      )
 
-# 훈련 세트와 테스트 세트로 나눕니다
+# 훈련 세트와 테스트 세트로 나눔
 train_input, test_input, train_target, test_target = train_test_split(
     perch_length, perch_weight, random_state=42)
-# 훈련 세트와 테스트 세트를 2차원 배열로 바꿉니다
+# 훈련 세트와 테스트 세트를 2차원 배열로 바꿈
 train_input = train_input.reshape(-1, 1)
 test_input = test_input.reshape(-1, 1)
 
@@ -32,4 +32,88 @@ knr = KNeighborsRegressor(n_neighbors=3)
 # k-최근접 이웃 회귀 모델을 훈련
 knr.fit(train_input, train_target)
 
+# 50cm 농어 무게 예측
 print(knr.predict([[50]]))
+
+# 50cm 농어의 이웃을 구함
+distances, indexes = knr.kneighbors([[50]])
+
+import matplotlib.pyplot as plt
+
+# 샘플 무게 평균 구함
+print(np.mean(train_target[indexes]))
+
+# 100cm 농어 무게 예측
+print(knr.predict([[100]]))
+
+# 100cm 농어의 이웃을 구함
+distances, indexes = knr.kneighbors([[100]])
+
+# 훈련 세트의 산점도를 그림
+plt.scatter(train_input, train_target)
+# 훈련 세트 중에서 이웃 샘플만 다시 그림
+plt.scatter(train_input[indexes], train_target[indexes], marker='D')
+# 100cm 농어 데이터
+plt.scatter(100, 1033, marker='^')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+
+# 선형 회귀
+from sklearn.linear_model import LinearRegression
+
+lr = LinearRegression()
+# 선형 회귀 모델 훈련
+lr.fit(train_input, train_target)
+
+# 50cm 농어에 대한 예측
+print(lr.predict([[50]]))
+
+# lr 객체의 기울기와 절편 출력
+print(lr.coef_, lr.intercept_)
+
+# 훈련 세트의 산점도를 그림
+plt.scatter(train_input, train_target)
+# 15에서 50까지 1차 방정식 그래프를 그림
+plt.plot([15, 50], [15*lr.coef_+lr.intercept_, 50*lr.coef_+lr.intercept_])
+# 50cm 농어 데이터
+plt.scatter(50, 1241.8, marker='^')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+print(lr.score(train_input, train_target)) # 훈련 세트
+print(lr.score(test_input, test_target)) # 테스트 세트
+
+
+# 다항 회귀
+# np.column_stack()를 사용해서 길이 제곱을 붙임
+train_poly = np.column_stack((train_input ** 2, train_input))
+test_poly = np.column_stack((test_input ** 2, test_input))
+
+# 데이터셋 크기 확인
+print(train_poly.shape, test_poly.shape)
+
+lr = LinearRegression()
+lr.fit(train_poly, train_target)
+
+print(lr.predict([[50**2, 50]]))
+
+print(lr.coef_, lr.intercept_)
+
+# 구간별 직선을 그리기 위해 15에서 49까지 정수 배열을 만듦
+point = np.arange(15, 50)
+# 훈련 세트의 산점도를 그림
+plt.scatter(train_input, train_target)
+# 15에서 49까지 2차 방정식 그래프를 그림
+plt.plot(point, 1.01*point**2 - 21.6*point + 116.05)
+# 50cm 농어 데이터
+plt.scatter([50], [1574], marker='^')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+# 훈련 세트와 테스트 세트 점수
+print(lr.score(train_poly, train_target))
+print(lr.score(test_poly, test_target))
