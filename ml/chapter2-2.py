b0c78281e4bc0aaf0e7395eecc011b1747e55d39
kth232 <kth7537@naver.com>
2025-03-07 17:44:09 +0900

    데이터 전처리

--- ml/chapter2-2.py
+++ ml/chapter2-2.py
@@ -24,21 +24,89 @@ print(fish_target)
 
 
 # 사이킷런으로 훈련 세트와 테스트 세트 나누기
-# from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 
-# train_input, test_input, train_target, test_target = train_test_split(
-#     fish_data, fish_target, random_state=42)
+# train_test_split(): 비율에 맞게 훈련 세트, 테스트 세트로 나눠주는 함수
+# random_state 매개변수로 랜덤시드 지정
+# stratify 매개변수에 타겟 데이터 전달하면 클래스 비율에 맞게 데이터 나눠줌
+train_input, test_input, train_target, test_target = train_test_split(
+    fish_data, fish_target, stratify=fish_target, random_state=42)
+
+# 넘파이 배열은 튜플로 표현
+print(train_input.shape, test_input.shape)
+
+print(train_target.shape, test_target.shape)
+
+print(test_target)
 
 
-# print(train_input.shape, test_input.shape)
+from sklearn.neighbors import KNeighborsClassifier
 
+kn = KNeighborsClassifier()
+kn.fit(train_input, train_target)
+kn.score(test_input, test_target)
 
-# print(train_target.shape, test_target.shape)
+print(kn.predict([[25, 150]]))
 
-# print(test_target)
+import matplotlib.pyplot as plt
 
+# kneighbors(): 이웃까지의 거리와 이웃 샘플 인덱스를 반환
+distances, indexes = kn.kneighbors([[25, 150]])
 
-# train_input, test_input, train_target, test_target = train_test_split(
-#     fish_data, fish_target, stratify=fish_target, random_state=42)
+plt.scatter(train_input[:,0], train_input[:,1])
+plt.scatter(25, 150, marker='^')
+plt.scatter(train_input[indexes,0], train_input[indexes,1], marker='D')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
 
-# print(test_target)
+print(train_input[indexes])
+
+print(train_target[indexes])
+
+print(distances)
+
+plt.scatter(train_input[:,0], train_input[:,1])
+plt.scatter(25, 150, marker='^')
+plt.scatter(train_input[indexes,0], train_input[indexes,1], marker='D')
+plt.xlim((0, 1000)) # xlim(): x축의 범위를 지정하는 함수
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+# axis=0으로 설정하여 행을 따라 각 열의 통계 값을 계산함
+mean = np.mean(train_input, axis=0) # 평균 계산
+std = np.std(train_input, axis=0) # 표준편차 계산
+
+print(mean, std)
+
+# 표준점수 = (원본 데이터 - 평균)/표준편차
+train_scaled = (train_input - mean) / std
+
+# 샘플도 평균과 표준편차를 이용해서 동일한 비율로 변환
+new = ([25, 150] - mean) / std
+
+plt.scatter(train_scaled[:,0], train_scaled[:,1])
+plt.scatter(new[0], new[1], marker='^')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+# 전처리한 데이터셋으로 훈련
+kn.fit(train_scaled, train_target)
+
+# 테스트 세트도 훈련 세트처럼 평균과 표준편차로 스케일 변환해야 함
+test_scaled = (test_input - mean) / std
+
+kn.score(test_scaled, test_target)
+
+print(kn.predict([new]))
+
+distances, indexes = kn.kneighbors([new])
+
+plt.scatter(train_scaled[:,0], train_scaled[:,1])
+plt.scatter(new[0], new[1], marker='^')
+plt.scatter(train_scaled[indexes,0], train_scaled[indexes,1], marker='D')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
